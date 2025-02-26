653dd7769c120b019e1ef48d351173579f4762f3
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2025-02-26 11:51:52 +0600

    style the displying name

--- src/components/DisplayUsers.jsx
+++ src/components/DisplayUsers.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import "./DisplayUsers.css";
 
 function DisplayUsers() {
     const users = useSelector((state) => state.users);
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    const handleClick = (user) => {
+        setSelectedUser(user); // Set the clicked user
+    };
 
     return (
-        <div className="display-container">
-            
+        <div className="display-container2">
             <ul>
                 {users.map((user) => (
-                    <li key={user.id}>{user}</li>
+                    <li
+                        key={user.id}
+                        className={`unique-li ${selectedUser === user ? "active-user" : ""}`}
+                        onClick={() => handleClick(user)} // Handle click event
+                    >
+                        <p className="unique-text text-gray-500">
+                            {user} {/* Assuming the user object has a 'name' field */}
+                        </p>
+                    </li>
                 ))}
             </ul>
+
+          
         </div>
     );
 }
